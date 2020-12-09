9e37a6b66d71b6379b53458f6bdaa761097b4da1
Aida Real <areal060781@gmail.com>
2020-12-09 18:01:06 -0500

    Add an alternative solution to bubble sort

--- src/bubbleSort.php
+++ src/bubbleSort.php
@@ -2,15 +2,31 @@
 
 function bubbleSort($array)
 {
+    $len = count($array);
     $toOrder = true;
     while ($toOrder) {
         $toOrder = false;
-        for ($i = 1; $i < count($array); $i++) {
+        for ($i = 1; $i < $len; $i++) {
             if ($array[$i - 1] > $array[$i]) {
                 $temp = $array[$i];
                 $array[$i] = $array[$i - 1];
                 $array[$i - 1] = $temp;
                 $toOrder = true;
+            }
+        }
+    }
+    return $array;
+}
+
+function bubbleSort2($array)
+{
+    $len = count($array);
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = 0; $j < $len -1; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
             }
         }
     }
